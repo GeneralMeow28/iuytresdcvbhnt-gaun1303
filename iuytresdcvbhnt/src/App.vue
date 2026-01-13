30e558b252d23bb851b8001e2607074d9706741b
GeneralMeow28 <nicogau17@hotmail.com>
2026-01-12 22:19:02 -0500

    minimal criteria

--- iuytresdcvbhnt/src/App.vue
+++ iuytresdcvbhnt/src/App.vue
@@ -1,11 +1,16 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>iuytresdcvbhnt</h1>
+  <footer>
+    <h3>Registre des changements</h3>
+    <ul>
+      <li>
+        <p>1. Page initiale avec les requis minimaux (2026-01-12)</p>
+      </li>
+    </ul>
+    <p>Nicolas Gaumond - gaun1303</p>
+  </footer>
 </template>
 
 <style scoped></style>
